1d0d7063cbd75c2c4b43cea04802ae77717ec521
Lucas Alexandre <lucasalexandre.monteirodf@gmail.com>
2024-04-03 21:56:27 -0300

    mongodb

--- models/Produto.js
+++ models/Produto.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose')
+const { type } = require('os')
 
 const schema = mongoose.Schema({
-    nome: String,
-    peso: Number,
-    tamanho: String,
-    tipo: String,
+    nome: {type: String,
+        required: true,
+        uppercase: true,
+        maxLength: [9],
+        trim: true,
+    },
+    preco: {type: String,
+        lowercase: true,
+    },
+    tamanho: {type: String,
+        default: ('2 litros')
+    },
+    tipo: {type: String,
+        enum:["Bebidas", "Pizzas", "Massas", "Sobremesas"]
+    },
     ingredientes: []
 })
 
